5ae6c6bdf2785cddf031b3c8a4795ebabb686c08
unknown <ECO0453_KIENLT@SMPC-020>
2021-04-16 17:03:06 +0700

    first commit

---
+++ src/components/Home/Header/Header.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="header">
+    <div class="top-header">
+      <div>
+        <a href="#">
+          mon@mon.media
+        </a>
+        <a href="#">
+          093032123
+        </a>
+      </div>
+      <div>
+        <a href="#">
+          Dang nhap
+        </a>
+        <a href="#">
+          Dang ky
+        </a>
+        <a href="#">Gio hang</a>
+      </div>
+    </div>
+  </div>
+</template>
+<script lang="js" src="./Header.js"></script>
+<style lang="css" src="./Header.css"></style>
